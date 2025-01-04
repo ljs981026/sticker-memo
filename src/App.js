1710212c224494823b111a7cecf380bdbfccbb2a
ljs981026 <ljs981026@naver.com>
2025-01-05 02:04:42 +0900

    메모 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { useCallback } from 'react';
 
 function App({store}) {
   const AddMemo = useCallback(() => store.addMemo(), [store]);
+  const Edit = useCallback((id, content) => store.editMemo(id, content), [store])
   return (
     <>
     {
-      store.memos.map((item) => 
-        <Memo key={item.id}/>
+      store.memos.map((memo) => 
+        <Memo key={memo.id} item={memo} Edit={Edit}/>
       )
     }
     <AddIcon sx={{float: "right", backgroundColor: "#e4e4e4", borderRadius: "5px", cursor: "pointer", fontSize: "30px", border: "1px solid black"}} onClick={AddMemo}/>
--- src/Memo/Memo.jsx
+++ src/Memo/Memo.jsx
@@ -1,11 +1,20 @@
-import React, { useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import DragHandleIcon from "@mui/icons-material/DragHandle";
 import CloseIcon from "@mui/icons-material/Close";
 import "./Memo.scss";
 import Draggable from "@ljs981026/draggable";
+import { debounce } from "underscore";
 
 const Memo = ({item, Delete, Edit, SetPosition, SetWidthHight}) => {
   const handleRef = useRef(null);
+  const onChangeMemo = useMemo(() => debounce(e => Edit(item.id, e.target.value),500), [item.id, Edit]);
+
+  useEffect(() => {
+    return () => {
+      onChangeMemo.cancel()
+    }
+  },[onChangeMemo])
+
   return (
     <Draggable handleRef={handleRef} onMove={(x, y) => console.log(x, y)} x={0} y={0}>
       <div ref={handleRef} className="memo-container" style={{width: `${250}px`, height: `${300}px`}}>
@@ -13,7 +22,14 @@ const Memo = ({item, Delete, Edit, SetPosition, SetWidthHight}) => {
           <DragHandleIcon sx={{cursor: "move", fontSize: "25px"}} />
           <CloseIcon sx={{cursor: "pointer", fontSize: "25px", float: "right"}}/>
         </div>
-        <textarea className="memo-text-area" defaultValue={"Enter memo here"} name="txt" placeholder="Enter memo here"></textarea>
+        <textarea 
+          className="memo-text-area" 
+          defaultValue={item.content} 
+          name="txt" 
+          placeholder="Enter memo here"
+          onChange={onChangeMemo}
+        >          
+        </textarea>
       </div>
     </Draggable>
   )
